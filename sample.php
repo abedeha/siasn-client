5d87762ab8917c0498defb0ad41044bb9ea87e86
abedeha <68211354+abedeha@users.noreply.github.com>
2025-07-18 15:08:32 +0700

    Update sample.php

--- sample.php
+++ sample.php
@@ -4,9 +4,9 @@ require __DIR__ . '/vendor/autoload.php';
 
 use SiasnClient\SIASN_Core;
 
-$nip = '199101052015031001';
-$orangID = '0CA7CD787AAA7C23E050640A15023707';
-$filePath = 'peremajaan/usulan/000000006e640bf0016e651d9dda110c_20240807_052408_filepak-f94d895c-547c-11ef-9c11-0a580a8205cc.pdf';
+$nip = '1991xxxxxxxxxxxxxxxx';
+$orangID = 'xxxxxxxxxxxxxxxxxxxxxxx';
+$filePath = 'path dokumen';
 
 // non file
 $response = SIASN_Core::getRiwayatAngkaKredit($nip);
